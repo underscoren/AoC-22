d0b8d724db5b7363b1a9cff864c7e071ab808c82
underscoren <marius.steffens@outlook.com>
2022-12-02 14:44:47 +0000

    Day 1

---
+++ README.md
@@ -0,0 +1,8 @@
+# AoC 2022
+
+Using typescript esm modules
+
+Run using 
+```
+ts-node --esm (file.ts)
+```
---
+++ day1/index.ts
@@ -0,0 +1,23 @@
+import { readFile } from "fs/promises";
+
+const sum = (a,b) => a + b;
+
+const calorieList = (await readFile("input.txt")) // read file 
+    .toString("utf-8") // convert to string
+    .split("\n") // split into lines
+    .map(l => parseInt(l)) // convert lines to numbers (empty line is NaN)
+    .reduce((arr, current) => { // group full lines into arrays (now we have an array of arrays)
+        if(isNaN(current)) {
+            arr.push([]);
+            return arr;
+        }
+            
+        arr[arr.length-1].push(current)
+        return arr;
+    }, [[]])
+    .map(arr => arr.reduce(sum, 0)) // add up each inner array
+    .sort((a,b) => b-a); // sort array in descending order
+
+console.log("most calories: ", calorieList[0]);
+console.log("top 3: ", calorieList.slice(0,3).reduce(sum))
+
